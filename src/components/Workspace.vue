06c664869fb653b03d01c40738b739dda28de1a6
James Foster <github@jgfoster.net>
2018-08-27 22:36:49 -0700

    Code now displays!

--- src/components/Workspace.vue
+++ src/components/Workspace.vue
@@ -1,9 +1,11 @@
 <template>
   <v-container fluid>
     <v-layout row wrap>
-      <ace-editor v-model="code" min-lines="3" max-lines="30" width="500" height="100"></ace-editor>
-      <pre><code>{{ code }}</code></pre>
-      <textarea style="width: 100%;" rows="5" v-model="code"></textarea>
+      <div>
+        <ace-editor v-model="code" min-lines="3" max-lines="30"></ace-editor>
+        <pre><code>{{ code }}</code></pre>
+        <textarea style="width: 100%;" rows="5" v-model="code"></textarea>
+      </div>
     </v-layout>
   </v-container>
 </template>
